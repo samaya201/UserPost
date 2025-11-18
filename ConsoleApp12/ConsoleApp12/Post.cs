cf6c32a4ddb8da00a4908fc922dbfffcad9d34bd
samaya201 <samayaia-mpa201@code.edu.az>
2025-11-18 19:04:11 +0400

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12;
+
+class Post
+{
+   
+    public int Id;
+    public int UserId;
+    public string Title;
+    public string Body;
+    public bool IsDeleted = false;
+
+    /*public Post(int userId, string title, string body)
+    {
+        UserId = userId;
+        Title = title;
+        Body = body;
+      
+    }*/
+}
+//TASK
+/*"userId": 1,
+"id": 1,
+"title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+"body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+
+getId - method
+*/
---
+++ ConsoleApp12/ConsoleApp12/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12;
+
+
+class User
+{
+    
+    public int Id;
+    public string Name;
+    public string Username;
+    public string Email;
+    public string Phone;
+    public string Website;
+    public bool IsDeleted = false;
+    /*public User(string username, string name, string email, string phone, string website)
+    {
+        Username = username;
+        Name = name;
+        Email = email;
+        Phone = phone;
+        Website = website;
+        Id = _idDefault++;
+    }*/
+
+}
+/*
+ * "id": 1, - private
+"name": "Leanne Graham",
+"username": "Bret",
+"email": "Sincere@april.biz",
+"phone": "1-770-736-8031 x56442",
+"website": "hildegard.org",
+
+
+user ve post crud(create, read, udate, delete) => delete soft delete olmalidir.IsDeleted true/false olaraq dəyişməlisiniz.
+terminalda render edərkən isDeleted false olanlar görsənməlidir.*/
+
